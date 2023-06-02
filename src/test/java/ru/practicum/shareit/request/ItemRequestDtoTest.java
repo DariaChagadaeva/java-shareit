97528c24b0d0f78b19333ddbc8d811453c36efcf
Daria Chagadaeva <chagadaeva17@yandex.ru>
2023-06-02 18:47:42 +0300

    fix: checkstyle

--- src/test/java/ru/practicum/shareit/request/ItemRequestDtoTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestDtoTest.java
@@ -9,7 +9,6 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
